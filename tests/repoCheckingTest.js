05537349e9bdafe0e9dad591a8180e782352b362
Mridul kumar <mridulkumar@mriduls-mbp.home>
2017-01-08 11:41:06 +0000

    fixed syntax error with test case.

--- tests/repoCheckingTest.js
+++ tests/repoCheckingTest.js
@@ -11,7 +11,7 @@ describe('githubClient', function() {
     var repos = [{language: "javascript"}, {language: "javascript"},
                 {language: "java"}, {language: "python"}]
     it("should give javascript", function() {
-      expect(gbc.getRepos(JSON.parse(repos)));
+      expect(gbc.getRepos(JSON.parse(repos))).toEqual("javascript");
     })
   });
 

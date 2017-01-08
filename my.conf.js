b2d00e0c13d16c68dd739068667b14565c31ee5b
Mridul kumar <mridulkumar@mriduls-mbp.home>
2017-01-08 12:54:39 +0000

    added plugins

--- my.conf.js
+++ my.conf.js
@@ -58,6 +58,10 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
 
+    plugins: [
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits

e95135dd3f9a95a0ea6e50b79b088bd67e8c7a95
Mridul kumar <mridulkumar@mriduls-mbp.home>
2017-01-08 12:35:00 +0000

    Added another test case

--- tests/repoCheckingTest.js
+++ tests/repoCheckingTest.js
@@ -12,8 +12,14 @@ describe('githubClient', function() {
                 {language: "java"}, {language: "python"}];
 
     it("should give javascript", function() {
-      expect(gbc.getRepos(repos)[0]).toEqual("javascript");
+      expect(gbc.getRepos(repos)).toEqual(['javascript']);
     });
   });
 
+  var repos2 = [{language: 'javascript'},{language: 'javascript'},
+              {language: 'java'},{language: 'java'},{language: 'python'}];
+
+  it("should give array containg javascript and java", function() {
+      expect(gbc.getRepos(repos2)).toEqual(['javascript', 'java']);
+  });
 });

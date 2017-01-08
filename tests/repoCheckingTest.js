29e0772bb81bf4e12dc1278d00ccd3ea1ad28e57
Mridul kumar <mridulkumar@mriduls-mbp.home>
2017-01-08 11:37:20 +0000

    Added test case to check logic.

---
+++ tests/repoCheckingTest.js
@@ -0,0 +1,18 @@
+describe('githubClient', function() {
+
+  var gbc;
+
+  beforeEach(function() {
+      gbc = new githubClient();
+  });
+
+  describe('using gbc to extra most popular repos', function() {
+
+    var repos = [{language: "javascript"}, {language: "javascript"},
+                {language: "java"}, {language: "python"}]
+    it("should give javascript", function() {
+      expect(gbc.getRepos(JSON.parse(repos)));
+    })
+  });
+
+});
